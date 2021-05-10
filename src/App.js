ff6f54a1afea7e26a52fe316faf9fdbf8828431f
kanttanhed <plinio.salazar@yahoo.com.br>
2021-05-10 19:00:57 -0300

    Navbar Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import React from 'react';
+import Navbar from './components/navbar'
 
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+      <Navbar/>
+
     </div>
   );
 }
