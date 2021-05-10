e982bac747f45fc1c8edf6c6103688f7d38f3089
kanttanhed <plinio.salazar@yahoo.com.br>
2021-05-10 19:37:26 -0300

    edit home

---
+++ src/views/home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home(){
+    return(
+        <div className="jumbotron">
+            <h1 className="display-3">Ola</h1>
+            <p className="lead">
+            Seja bem vindo ao Sistema de Cadastro de Produtos</p>
+            <hr className="my-4"/>  
+            <p className="lead">
+                <a className="btn btn-primary btn-lg" href="www.google.com" role="button">Cadastrar</a>
+            </p>
+        </div>
+    )
+}
+
+export default Home;
