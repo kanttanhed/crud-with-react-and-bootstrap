94596cc552922760b46b561e40f21bbb8d3a3d9a
kanttanhed <plinio.salazar@yahoo.com.br>
2021-05-14 17:45:29 -0300

    Implementing function delete  button

--- src/views/produtos/consulta.js
+++ src/views/produtos/consulta.js
@@ -26,6 +26,11 @@ class ConsultaProdutos extends React.Component {
         this.props.history.push(`/cadastroProdutos/${sku}`)
     }
 
+    deletar = (sku) => {
+        const produtos = this.service.deletar(sku)
+        this.setState({produtos })
+    }
+
 
     render() {
         return (
@@ -59,7 +64,7 @@ class ConsultaProdutos extends React.Component {
                                             <th>{produto.fornecedor}</th>
                                             <th>
                                                 <button onClick={() => this.preparaEditar(produto.sku)} className="btn btn-primary">Editar</button>
-                                                <button className="btn btn-danger">Deletar</button>
+                                                <button onClick={() => this.deletar(produto.sku)}  className="btn btn-danger">Deletar</button>
                                             </th>
                                         </tr>
                                     )
